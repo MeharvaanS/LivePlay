c93560599c2e9d93b12930e5e9606d98d0158cf5
Meharvaan Singh <simeharv@sheridancollege.ca>
2024-05-05 11:20:26 -0400

    excessive API link removed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let showUpcomingButtonAdded = false; // Variable to track if "Show More" button 
 let currentLeague = "Indian Premier League"; // Variable to store the current league
 let showRecentButton; // Variable to store reference to "Show More" button for recent matches
 let showUpcomingButton; // Variable to store reference to "Show More" button for upcoming matches
-// return await fetch("https://api.cricapi.com/v1/cricScore?apikey=616efe51-c75d-4763-9ac5-44f81d322268")
+//616efe51-c75d-4763-9ac5-44f81d322268
 
 async function getMatchData(league) {
     // Fetching data 
@@ -266,3 +266,29 @@ function resetShowMoreButtons() {
 }
 
 getMatchData("Indian Premier League");
+dark();
+function dark() { 
+    // the css we are going to inject
+    var css = 'html {-webkit-filter: invert(100%);' +
+        '-moz-filter: invert(100%);' + 
+        '-o-filter: invert(100%);' + 
+        '-ms-filter: invert(100%); }',
+    
+    head = document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+    
+    // // a hack, so you can "invert back" clicking the bookmarklet again
+    // if (!window.counter) { window.counter = 1;} else  { window.counter ++;
+    // if (window.counter % 2 == 0) { var css ='html {-webkit-filter: invert(0%); -moz-filter:    invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }'}
+    //  };
+    
+    style.type = 'text/css';
+    if (style.styleSheet){
+    style.styleSheet.cssText = css;
+    } else {
+    style.appendChild(document.createTextNode(css));
+    }
+    
+    //injecting the css to the head
+    head.appendChild(style);
+    };
